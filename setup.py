822a92504748747c9f1324bfb7dba09d3bb9deeb
Rohan Nalla <rohannalla727@gmail.com>
2025-10-29 19:07:09 -0400

    Adding project to GitHub

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gitpulse",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "GitPython",
+        "rich"
+    ],
+    entry_points={
+        "console_scripts": [
+            "gitpulse=gitpulse.main:main",
+        ],
+    },
+)
